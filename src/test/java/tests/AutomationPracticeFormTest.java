2b1ddedb34c2c943e30dc6596c7112a2bd220aad
DESKTOP-NQBEUNQ\ivane <ivan.efryushkin@devexperts.com>
2026-09-18 18:05:37 +0300

    Corrections

--- src/test/java/tests/AutomationPracticeFormTest.java
+++ src/test/java/tests/AutomationPracticeFormTest.java
@@ -15,36 +15,34 @@ public class AutomationPracticeFormTest extends TestBase{
         $("[id=firstName]").setValue("Leon");
         $("[id=lastName]").setValue("Testionni");
         $("[id=userEmail]").setValue("leon@testionni.com");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("99999999999");
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
         $(".react-datepicker__year-select").selectOption("1988");
         $("div.react-datepicker__day.react-datepicker__day--006").click();
-        $(".subjects-auto-complete__input-container").click();
-        $(".subjects-auto-complete__input").setValue("E");
-        $(byText("English")).click();
-        $("[id=hobbies-checkbox-1]").click();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
         $("[id=uploadPicture]").uploadFromClasspath("my-image.png");
         $("[id=currentAddress]").setValue("Bullet street 7");
         $("#state").click();
-        $(byText("NCR")).click();
+        $("#react-select-3-listbox").$(byText("NCR")).click();
         $("#city").click();
-        $(byText("Delhi")).click();
+        $("#react-select-4-listbox").$(byText("Delhi")).click();
         $("[id=submit]").click();
 
         $(".modal-open").shouldBe(visible);
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-        $x("//td[text()='Student Name']/following-sibling::td").shouldHave(text("Leon Testionni"));
-        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(text("leon@testionni.com"));
-        $x("//td[text()='Gender']/following-sibling::td").shouldHave(text("Male"));
-        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(text("9999999999"));
-        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(text("06 August,1988"));
-        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(text("English"));
-        $x("//td[text()='Hobbies']/following-sibling::td").shouldHave(text("Sports"));
-        $x("//td[text()='Picture']/following-sibling::td").shouldHave(text("my-image.png"));
-        $x("//td[text()='Address']/following-sibling::td").shouldHave(text("Bullet street 7"));
-        $x("//td[text()='State and City']/following-sibling::td").shouldHave(text("NCR Delhi"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Leon Testionni"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("leon@testionni.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9999999999"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("06 August,1988"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("English"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("my-image.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Bullet street 7"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
         $("#closeLargeModal").shouldBe(visible);
     }
 
@@ -55,7 +53,7 @@ public class AutomationPracticeFormTest extends TestBase{
         $("[id=firstName]").setValue("Leon");
         $("[id=lastName]").setValue("Testionni");
         $("[id=userEmail]").setValue("leon@testionni.com");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("99999999999");
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
@@ -67,16 +65,16 @@ public class AutomationPracticeFormTest extends TestBase{
 
         $(".modal-open").shouldBe(visible);
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-        $x("//td[text()='Student Name']/following-sibling::td").shouldHave(text("Leon Testionni"));
-        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(text("leon@testionni.com"));
-        $x("//td[text()='Gender']/following-sibling::td").shouldHave(text("Male"));
-        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(text("9999999999"));
-        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(text("06 August,1988"));
-        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(exactText(""));
-        $x("//td[text()='Hobbies']/following-sibling::td").shouldHave(exactText(""));
-        $x("//td[text()='Picture']/following-sibling::td").shouldHave(text("my-image.png"));
-        $x("//td[text()='Address']/following-sibling::td").shouldHave(text("Bullet street 7"));
-        $x("//td[text()='State and City']/following-sibling::td").shouldHave(exactText(""));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Leon Testionni"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("leon@testionni.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9999999999"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("06 August,1988"));
+        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldBe(empty);
+        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldBe(empty);
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("my-image.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Bullet street 7"));
+        $(".table-responsive").$(byText("State and City")).sibling(0).shouldBe(empty);
         $("#closeLargeModal").shouldBe(visible);
     }
 
@@ -87,7 +85,7 @@ public class AutomationPracticeFormTest extends TestBase{
         $("[id=firstName]").setValue("Leon");
         $("[id=lastName]").setValue("Testionni");
         $("[id=userEmail]").setValue("leon@testionni.com");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("ffffffffffff");
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
@@ -108,7 +106,7 @@ public class AutomationPracticeFormTest extends TestBase{
         $("[id=firstName]").setValue("Leon");
         $("[id=lastName]").setValue("Testionni");
         $("[id=userEmail]").setValue("rewrerwr");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("9999999999");
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
@@ -128,7 +126,7 @@ public class AutomationPracticeFormTest extends TestBase{
         open("/automation-practice-form");
         $("[id=lastName]").setValue("Testionni");
         $("[id=userEmail]").setValue("leon@testionni.com");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("9999999999");
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
@@ -148,7 +146,7 @@ public class AutomationPracticeFormTest extends TestBase{
         open("/automation-practice-form");
         $("[id=firstName]").setValue("Leon");
         $("[id=userEmail]").setValue("leon@testionni.com");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("9999999999");
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
@@ -169,7 +167,7 @@ public class AutomationPracticeFormTest extends TestBase{
         $("[id=firstName]").setValue("Leon");
         $("[id=lastName]").setValue("Testionni");
         $("[id=userEmail]").setValue("leon@testionni.com");
-        $("[id=gender-radio-1]").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=dateOfBirthInput]").click();
         $("[class=react-datepicker__month-select]").selectOption(7);
         $(".react-datepicker__year-select").selectOption("1988");
